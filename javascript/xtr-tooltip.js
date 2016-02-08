b99d4e897a6a99c8280db04dadc510dbd636752d
Yuri Marinho Alcântara <yuri.maralca@outlook.com>
2016-02-08 14:37:05 -0300

    Fixed position and assembling with id

--- javascript/xtr-tooltip.js
+++ javascript/xtr-tooltip.js
@@ -16,6 +16,7 @@ function XtrTooltip(id,direction){
 
 	if(xtrTooltip == null){
 		xtrTooltip = document.createElement("div");
+		xtrTooltip.setAttribute("id",tooltipId);
 		document.body.appendChild(xtrTooltip);
 	}
 
@@ -36,7 +37,7 @@ function XtrTooltip(id,direction){
 		defaultClass = "xtrTooltip-"+direction;
 		xtrTooltip.setAttribute("class","xtrTooltip "+defaultClass);
 	}	
-	function addTrigger(triggerId,triggerObject){
+	function addTrigger(triggerselector,triggerObject){
 		var triggerId;
 		var triggerElement;
 
@@ -59,53 +60,65 @@ function XtrTooltip(id,direction){
 				offset = {};
 			}
 		}
-		if(triggerId instanceof Node)
-			triggerElement = triggerId;
-		else
-			triggerElement = document.getElementById(triggerId);
+		if(triggerselector instanceof Node){
+			triggerElements = triggerselector;
+			triggerElement = triggerselector;
+			exception = true;
+		}
+		else{
+			exception = false;
+			triggerElements = document.querySelectorAll(triggerselector);
+		}
 
-		if(triggerElement == null){
+		if(triggerElements == null){
 			console.warn("XtrTooltip, trigger id does not exist");
 			return;
 		}
-		triggerElement.addEventListener("mouseover",function(event){
+		
+		for(triggerElementIndex = 0; triggerElements.length > triggerElementIndex || exception; triggerElementIndex++){
+			if(!exception)
+				triggerElement = triggerElements[triggerElementIndex];
+			exception = false;
 
-			xtrTooltip.innerHTML = HTML;
+			triggerElement.addEventListener("mouseover",function(event){
 
-			boundingElement = this.getBoundingClientRect();
-			boundingTooltip = xtrTooltip.getBoundingClientRect();
-			if(direction == "right"){
-				moveX = boundingElement.left - boundingTooltip.width - 10;
-				moveY = boundingElement.top + (boundingElement.height - boundingTooltip.height)/2;
-			}
-			else if(direction == "left"){
-				moveX = boundingElement.right + 10;
-				moveY = boundingElement.top + (boundingElement.height - boundingTooltip.height)/2;
-			}
-			else if(direction == "top"){
-				moveX = boundingElement.left + (boundingElement.width - boundingTooltip.width)/2;
-				moveY = boundingElement.bottom + 10;
-			}
-			else if(direction == "bottom"){
-				moveX = boundingElement.left + (boundingElement.width - boundingTooltip.width)/2;
-				moveY = boundingElement.top - boundingTooltip.height - 10;
-			}
+				xtrTooltip.innerHTML = HTML;
 
-			moveX += XtrGraficoUtil.isset(offset.x) ? offset.x : 0;
-			moveY += XtrGraficoUtil.isset(offset.y) ? offset.y : 0;
+				boundingElement = this.getBoundingClientRect();
+				boundingTooltip = xtrTooltip.getBoundingClientRect();
+				if(direction == "right"){
+					moveX = boundingElement.left - boundingTooltip.width - 10;
+					moveY = boundingElement.top + (boundingElement.height - boundingTooltip.height)/2;
+				}
+				else if(direction == "left"){
+					moveX = boundingElement.right + 10;
+					moveY = boundingElement.top + (boundingElement.height - boundingTooltip.height)/2;
+				}
+				else if(direction == "top"){
+					moveX = boundingElement.left + (boundingElement.width - boundingTooltip.width)/2;
+					moveY = boundingElement.bottom + 10;
+				}
+				else if(direction == "bottom"){
+					moveX = boundingElement.left + (boundingElement.width - boundingTooltip.width)/2;
+					moveY = boundingElement.top - boundingTooltip.height - 10;
+				}
 
-			moveX += "px";
-			moveY += "px";
+				moveX += XtrGraficoUtil.isset(offset.x) ? offset.x : 0;
+				moveY += XtrGraficoUtil.isset(offset.y) ? offset.y : 0;
 
-			xtrTooltip.style.setProperty("left",moveX);
-			xtrTooltip.style.setProperty("top",moveY);
+				moveX += "px";
+				moveY += "px";
 
-			xtrTooltip.setAttribute("class","xtrTooltip "+defaultClass+" show");
-		});
+				xtrTooltip.style.setProperty("left",moveX);
+				xtrTooltip.style.setProperty("top",moveY);
 
-		triggerElement.addEventListener("mouseout",function(){
-			xtrTooltip.setAttribute("class","xtrTooltip "+defaultClass);
-			xtrTooltip.style = "";
-		});	
+				xtrTooltip.setAttribute("class","xtrTooltip "+defaultClass+" show");
+			});
+
+			triggerElement.addEventListener("mouseout",function(){
+				xtrTooltip.setAttribute("class","xtrTooltip "+defaultClass);
+				xtrTooltip.style = "";
+			});	
+		}
 	}
 }
